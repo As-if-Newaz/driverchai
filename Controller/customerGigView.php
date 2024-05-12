8e1d999c6e01b9c5c6cc4602bc84aad7f4ec79ff
TRK34EMC <asif.kravmaga@gmail.com>
2024-05-13 00:51:57 +0600

    first commit

---
+++ Controller/customerGigView.php
@@ -0,0 +1,18 @@
+<?php
+session_abort();
+session_start();
+require '../Model/gig.php';
+require '../Model/job.php';
+$allAvailableGigs = getAllAvailableGigs();
+if(isset($_POST['request']))
+{
+    $g_id = $_POST['g_id'];
+    $status = "pending";
+    $c_id = $_SESSION['userId'];
+    createNewJob($g_id, $c_id, $status);
+    $d_id = getGigCreator($g_id);
+    $j_id = getCurrentJobId();
+    bindDriverWithJob($j_id, $d_id, $c_id);
+    header('Location: http://localhost/project/View/hired.php');
+}
+?>
